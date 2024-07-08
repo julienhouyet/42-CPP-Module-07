ca30cc88a154b6239b44c45107b023ed1a0459c1
julienhouyet <julien.houyet@gmail.com>
2024-07-08 09:59:44 +0200

    chore: Update whatever.hpp and main.cpp

--- ex00/include/whatever.hpp
+++ ex00/include/whatever.hpp
@@ -1,12 +1,36 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Whatever.hpp                                       :+:      :+:    :+:   */
+/*   whatever.hpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jhouyet <jhouyet@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/30 13:42:54 by jhouyet           #+#    #+#             */
-/*   Updated: 2024/06/30 13:42:55 by jhouyet          ###   ########.fr       */
+/*   Updated: 2024/07/08 09:50:41 by jhouyet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef FUNCTIONS_HPP
+#define FUNCTIONS_HPP
+
+template <typename T>
+void swap(T& a, T& b)
+{
+    T temp = a;
+    a = b;
+    b = temp;
+}
+
+template <typename T>
+T min(const T& a, const T& b)
+{
+    return (a < b) ? a : b;
+}
+
+template <typename T>
+T max(const T& a, const T& b)
+{
+    return (a > b) ? a : b;
+}
+
+#endif
--- ex00/src/main.cpp
+++ ex00/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jhouyet <jhouyet@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/30 13:43:00 by jhouyet           #+#    #+#             */
-/*   Updated: 2024/06/30 13:45:08 by jhouyet          ###   ########.fr       */
+/*   Updated: 2024/07/08 09:59:33 by jhouyet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,49 @@
 
 int	main()
 {
-	std::cout << "Hello Whatever" << std::endl;
+	int a = 1;
+	int b = 20;
+	
+	std::cout << "Values : a = " << a << ", b = " << b << std::endl;
+
+	std::cout << std::endl;
+    
+	swap(a, b);
+	
+	std::cout << "Swapping : a = " << a << ", b = " << b << std::endl;
+
+	std::cout << std::endl;
+    
+	std::cout << "Min: " << min(a, b) << std::endl;
+
+	std::cout << std::endl;
+	
+	std::cout << "Max: " << max(a, b) << std::endl;
+
+	std::cout << std::endl;
+	
+	std::cout << "----------------------------" << std::endl;
+
+	std::cout << std::endl;
+
+	double x = 1.1;
+	double y = 2.2;
+	
+	std::cout << "Values : x = " << x << ", y = " << y << std::endl;
+
+	std::cout << std::endl;
+    
+	swap(x, y);
+	
+	std::cout << "Swapping : x = " << x << ", y = " << y << std::endl;
+
+	std::cout << std::endl;
+    
+	std::cout << "Min: " << min(x, y) << std::endl;
+
+	std::cout << std::endl;
+	
+	std::cout << "Max: " << max(x, y) << std::endl;
+
+	return 0;
 }
